0192966b680f15c1c252e7056833270f71f4b396
Imre Korf <mc.imre@hotmail.com>
2019-02-13 11:08:45 +0100

    removed grid_overlay from designer.cs

--- Domotica ASP/default.aspx.designer.cs
+++ Domotica ASP/default.aspx.designer.cs
@@ -31,15 +31,6 @@ namespace Domotica {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl grid_overlay_control;
         
         /// <summary>
-        /// grid_overlay control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl grid_overlay;
-        
-        /// <summary>
         /// overlay_child control.
         /// </summary>
         /// <remarks>
