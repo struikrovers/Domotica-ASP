fc1ded959ce6e84ae954c33096bc053a736c9776
Joost Visserman <joost.trash@gmail.com>
2019-03-24 18:04:41 +0100

    making a submit buttton O.o

--- Domotica ASP/admin.aspx.cs
+++ Domotica ASP/admin.aspx.cs
@@ -21,6 +21,45 @@ namespace Domotica_ASP
                 Label1.Text = error5;
             }
             */
+            MySqlCommand userQuery = new MySqlCommand("SELECT gebruikersnaam, voornaam, achternaam FROM user WHERE gebruikersnaam != :gbnaam");
+            userQuery.Parameters.Add("gbnaam", Session["user"]);
+            List<List<string>> result = global.ExecuteReader(userQuery, out string apparaatError, out bool apparaatErrorInd);
+            if (apparaatErrorInd)
+            {
+                Label label = new Label();
+                label.Text = apparaatError;
+                grid_parent.Controls.Add(label);
+            }
+            else
+            {
+                foreach (List<string> row in result)
+                {
+                    Widget widget = (Widget)LoadControl("Widget.ascx");
+                    widget.name = row[0];
+                    widget.comment = string.Format("Dit is het account van: {0} {1}", row[1], row[2]);
+                    widget.toggle = true;
+                    Remove_User.Content.Controls.Add(widget);
+                }
+
+                Widget SubmitWidget = (Widget)LoadControl("Widget.ascx");
+                SubmitWidget.ID = "SubmitWidget";
+
+                PlaceHolder phInput = new PlaceHolder();
+                phInput.ID = "placeholder";
+
+                InputFields Input = (InputFields)LoadControl("InputFields.ascx");
+                Input.in_type = "radio";
+                Input.button_text = "submit";
+                Input.ID = "InputField";
+
+                phInput.Controls.Add(Input);
+
+                SubmitWidget.Input = phInput;
+
+                Remove_User.Content.Controls.Add(SubmitWidget);
+                
+            }
+
         }
     }
 }
